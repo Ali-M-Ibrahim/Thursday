1b3b3658ac481707f2ac0919a1b37ec97c0d3c90
ali <ali-ibrahim94@hotmail.com>
2024-03-14 17:53:45 +0200

    adding

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -70,4 +70,149 @@ class ProductController extends Controller
             ->delete();
     }
 
+    public function getAll(){
+        $obj = Product::all(); // select * from products;
+        return $obj;
+    }
+
+    public function getByName($name){
+        $obj = Product::where('name',$name)->get(); // select * from products where name=$name;
+        return $obj;
+    }
+
+    public function getByNameAndPrice($name,$price){
+        $obj = Product::where('name',$name)
+            ->where('price',$price)
+            ->get(); // select * from products where name=$name and price = $price;
+        return $obj;
+    }
+
+    public function getByNameAndPriceGreater($name,$price){
+        $obj = Product::where('name',$name)
+            ->where('price','>=' ,$price)
+            ->get(); // select * from products where name=$name and price = $price;
+        return $obj;
+    }
+
+
+    public function getFirstByNameAndPriceGreater($name,$price){
+        $obj = Product::where('name',$name)
+            ->where('price','>=' ,$price)
+            ->first(); // select * from products where name=$name and price = $price limit 1;
+        return $obj;
+    }
+
+
+    public function getFirstProduct($name){
+        $obj = Product::firstWhere('name',$name);
+//        $obj = Product::where('name',$name)->first();
+        return $obj;
+    }
+
+    public function getProductById($id){
+        $obj = Product::find($id); // select *  from products where id=$id;
+        return $obj;
+    }
+
+    public function getByNameOrPrice($name,$price){
+        $obj = Product::where('name',$name)
+            ->Orwhere('price',$price)
+            ->get(); // select * from products where name=$name or price = $price;
+        return $obj;
+    }
+
+    public function getProductBySeveralId($id1,$id2,$id3){
+        $obj = Product::where('id',$id1)
+        ->orWhere('id',$id2)
+            ->orWhere('id',$id3)->get()
+        ; // select *  from products where id=$id11 or id=$id2 or id=$id3;
+        return $obj;
+    }
+
+    public function getProductInSeveralId($id1,$id2,$id3){
+        $obj = Product::whereIn('id',[$id1,$id2,$id3])->get() ;
+        // select *  from products where id in ($id1,$id2,$id3);
+        return $obj;
+    }
+
+    public function getProductWherePriceBetween($price1,$price2){
+        $obj = Product::whereBetween('price',[$price1,$price2])
+         ->get()
+        ; // select *  from products where id=$id11 or id=$id2 or id=$id3;
+        return $obj;
+    }
+
+    public function getCountWherePriceBetween($price1,$price2){
+        $obj = Product::whereBetween('price',[$price1,$price2])
+            ->count()
+        ; // select count(*)  from products where price between $price1 and $price2
+        return $obj;
+    }
+
+    public function getMinIdWherePriceBetween($price1,$price2){
+        $obj = Product::whereBetween('price',[$price1,$price2])
+            ->min('id')
+        ; // select  min(id) from products where price between $price1 and $price2
+        return $obj;
+    }
+
+    public function getMaxIdWherePriceBetween($price1,$price2){
+        $obj = Product::whereBetween('price',[$price1,$price2])
+            ->max('id')
+        ; // select  min(id) from products where price between $price1 and $price2
+        return $obj;
+    }
+
+
+    public function getAvgIdWherePriceBetween($price1,$price2){
+        $obj = Product::whereBetween('price',[$price1,$price2])
+            ->avg('price')
+        ; // select  avg(price) from products where id=$id11 or id=$id2 or id=$id3;
+        return $obj;
+    }
+
+
+    public function getProductWherePriceBetweenSelect($price1,$price2){
+        $obj = Product::whereBetween('price',[$price1,$price2])
+            ->select(['name','description'])
+            ->get()
+        ; // select name,description  from products where id=$id11 or id=$id2 or id=$id3;
+        return $obj;
+    }
+
+
+
+    public function getElementByIdOrFail($id){
+        $obj = Product::findOrFail($id);
+        return $obj;
+    }
+
+
+    public function getFirstProductOrFail($name){
+        $obj = Product::where('name',$name)
+            ->firstOrFail(); // select * from products where name=$name and price = $price limit 1;
+        return $obj;
+    }
+
+    public function getFirstProductOr($name){
+        $obj = Product::where('name',$name)
+            ->firstOr(function(){
+                return "hi";
+            }
+            );
+        return $obj;
+    }
+
+
+    public function getProductBySeveralIdOrderByDesc($id1,$id2,$id3){
+        $obj = Product::where('id',$id1)
+            ->orWhere('id',$id2)
+            ->orWhere('id',$id3)
+            ->orderBy('id','desc')
+            ->get()
+        ; // select *  from products where id=$id11 or id=$id2 or id=$id3;
+        return $obj;
+    }
+
+
 }
